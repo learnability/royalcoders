ca30d04fdae0fbe64fead1d607eeee7983cbfbf2
amithp27 <amithp27@gmail.com>
2014-02-15 12:10:10 +0530

    multiply

---
+++ multiply.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//funtion for multiplication
+
+
+
+int multi(int a,int b)
+{
+
+return a*b;
+
+}
+
+int main()
+{
+int a,b;
+printf("Enter two values:- ");
+scanf("%d %d",&a,&b);
+
+printf("\nMULTIPLICATION IS:- %d\n",multi(a,b));
+
+}
